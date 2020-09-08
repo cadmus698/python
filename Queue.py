cccb38bea3503aa297f0af539ecd3dc72df6f995
codeboy93 <44343531+codeboy93@users.noreply.github.com>
2020-09-08 13:52:55 -0700

    Add files via upload

--- Queue.py
+++ Queue.py
@@ -1,10 +1,21 @@
 queuestr = input('What\'s the queue?(Seperate with commas)\n')
 queue = queuestr.split(',')
+queue1 = []
 while True:
     todo = input('What do you want to do\n')
     if todo == 'dequeue' or todo == 'Dequeue':
         print(queue[0])
-        queue.remove(queue[0])
+        num = 1
+        toend = False
+        while True:
+            if toend == True:
+                break
+            if queue[num] == queue[-1]:
+                toend = True
+            queue1.append(queue[num])
+            num += 1
+        queue = queue1
+        pass
     if todo == 'Add' or todo == 'add':
         toadd = input('What should I add?\n')
         queue.append(toadd)
